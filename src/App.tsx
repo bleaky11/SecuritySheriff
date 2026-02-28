e5a5d216649d6150547bacacdcd696451588665a
bleaky11 <levichen533@gmail.com>
2026-02-28 14:15:54 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import alien from "./assets/cowboyalien.png"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div /**Background here */>
+          <img src={alien} alt='alien cowboy'></img>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
