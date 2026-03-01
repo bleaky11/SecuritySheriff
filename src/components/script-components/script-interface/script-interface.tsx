256ec0d243a11b4d3ac00eb3b86ad7796197053b
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2026-03-01 09:35:08 -0500

    began implementing script interface

---
+++ src/components/script-components/script-interface/script-interface.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import type { Script } from "../../../data/models";
+
+export interface ScriptInterfaceProps {
+    script : Script
+}
+
+export function ScriptInterface({script} : ScriptInterfaceProps) {
+    
+    const [currentScript, setCurrentScript] = useState<Script>(script);
+    const [debugMode, setDebugMode] = useState<boolean>(false);
+    
+    return (
+        <div>
+            <button onClick={() => setDebugMode(!debugMode)}>Toggle Debug Mode</button>
+
+            {debugMode && 
+                <div> 
+                    <h2>Debug Information</h2>
+                    {
+                        currentScript.errors.map((error, index) => (
+                            <div key={index}>
+                                <p>Error Type: {error.errorType}</p>
+                                <p>Error Line: {error.line}</p>
+                                <p>Error Description: {error.description}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            } 
+        </div>
+    );
+}
