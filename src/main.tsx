b1bb6618f1ad1a2829d9eb633e30e7c390bc0539
bleaky11 <levichen533@gmail.com>
2026-02-28 22:23:30 -0500

    Finished all the visuals for now

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import SecuritySheriff from './SecuritySheriff.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path='/game' element={<SecuritySheriff/>}/>
+      </Routes>
+    </Router>
+  </StrictMode>
 )
